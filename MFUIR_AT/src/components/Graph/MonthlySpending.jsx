710404a38bc96391012c9f78a6a120948471b05c
Alberto Pluecker <albertopluecker@gmail.com>
2023-04-07 20:23:09 +0200

    Added bar graph, more responsiveness

--- MFUIR_AT/src/components/Graph/MonthlySpending.jsx
+++ MFUIR_AT/src/components/Graph/MonthlySpending.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import SectionTitle from '../SectionTitle'
+import chart from '../../assets/chart.svg'
+import styles from './MonthlySpending.module.css'
 
 function MonthlySpending() {
 	return (
-		<>
+		<div className={styles.sectionContainer}>
 			<SectionTitle title='Monthly Spending' />
-		</>
+			<div className={styles.subsectionContainer}>
+				<div className={styles.graphContainer}>
+					<img
+						src={chart}
+						className={styles.img}
+					></img>
+				</div>
+			</div>
+		</div>
 	)
 }
 
